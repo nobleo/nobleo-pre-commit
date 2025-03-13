cfc823f6e4baf9480e1219e8f860ffaf9aa0cfe6
Tim Clephas <tim.clephas@nobleo.nl>
2025-03-13 11:02:44 +0100

    If bumpver is used, make sure all package versions are bumpable

---
+++ pre_commit_hooks/verify_bumpver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from bumpver.cli import cli
+from click.testing import CliRunner
+
+
+def main():
+    runner = CliRunner()
+    # Try to bump minor version, --minor will be ignored if calver is used
+    result = runner.invoke(cli, ['update', '--minor', '--dry', '--no-fetch'])
+    if result.exit_code != 0 and 'File not found' not in result.output:
+        print(result.output)
+        return result.exit_code
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
